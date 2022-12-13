b048f23cbc38a35038671f68b7dde5545d43056a
Revati <revati.v.bordawekar@gmail.com>
2022-12-12 21:26:49 -0800

    code working for success

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+
 const submitBtn = document.getElementById('submit');
 const fname = document.getElementById('fname');
 const lname = document.getElementById('lname');
 const checkbox = document.getElementById('checkbox');
 const email = document.getElementById('email');
+const textarea = document.getElementById('textarea');
+
 
 const handleSubmit = () => {
     if(fname.value.length >=1 && lname.value.length >=1){
@@ -10,13 +13,62 @@ const handleSubmit = () => {
     }
 }
 
-fname.addEventListener('click', handleSubmit);
-lname.addEventListener('click', handleSubmit);
+function clearInputs(){
+    fname.value = "";
+    lname.value = "";
+    textarea.value = "";
+    checkbox.checked = false;
+    email.style.display = 'none'
+}
 
-submitBtn.addEventListener('click', () => {
-    alert(`My name is ${fname.value} ${lname.value}`)
-})
+fname.addEventListener('change', handleSubmit);
+lname.addEventListener('change', handleSubmit);
 
 checkbox.addEventListener('click', () => {
-    email.style.display = 'none' ? 'inline-block': 'none'
+  email.style.display = 'none' ? 'inline-block': 'none'
 })
+
+submitBtn.addEventListener('click', (e) => {
+   
+    e.preventDefault();
+    const data = {  
+
+          firstName: fname.value, 
+          lastName: lname.value, 
+          isSubscribed: checkbox.checked ? true: false, 
+          email: checkbox.checked ? email.value : null, 
+          comment: textarea.value
+           
+     };
+        
+        fetch('https://jsonplaceholder.typicode.com/users', {
+          method: 'POST', 
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+          console.log('Success:', data);
+          let text =    `Thanks for your submission ${data.firstName}`;
+          document.getElementById('message').innerHTML = text;
+     
+            setTimeout(() => {
+                const message = document.getElementById('message');
+                message.style.display = "none";
+              }, 5000); 
+
+              clearInputs();
+          })
+          .catch((error) => {
+            let text= "Oops something went wrong";
+            document.getElementById('message').innerHTML = text;
+            console.error('Error:', error);
+          });
+
+})
+
+
+
+
